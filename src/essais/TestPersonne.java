e126319e0f809846382b32e6b8aae08857bd97bb
Charles Bourtoire <charlesbourtoire@gmail.com>
2021-02-08 14:53:00 +0100

    petit correctif pour grater deux lignes de codes

--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -5,10 +5,7 @@ import entites.Personne;
 
 public class TestPersonne {
     public static void main(String[] args) {
-        AdressePostale a1 = new AdressePostale(11, "Grande rue", "78660", "Saint-Martin");
-        AdressePostale a2 = new AdressePostale(12, "Petite rue", "78660", "Saint-Martine");
-
-        Personne Elodie = new Personne("Casciola", "Elodie", a1);
-        Personne Charles = new Personne("Bourtoire", "Charles", a2);
+        Personne Elodie = new Personne("Casciola", "Elodie", new AdressePostale(11, "Grande rue", "78660", "Saint-Martin"));
+        Personne Charles = new Personne("Bourtoire", "Charles", new AdressePostale(12, "Petite rue", "78660", "Saint-Martine"));
     }
 }
